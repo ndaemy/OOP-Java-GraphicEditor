5c130130bb4eff824bc5bc66f8ea1013e8dfd05d
Workaholic <workaholic@Workaholicui-MacBook-Pro.local>
2017-09-05 16:32:52 +0900

    First commit

---
+++ src/constants/GEConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class GEConstants {
+	
+	//GEMainFrame
+	public static final int WDITH_MAINFRAME = 400;
+	public static final int HEIGHT_MAINFRAME = 600;
+	public static final String TITLE_MAINFRAME = "GE01";
+	
+}
---
+++ src/frames/GELauncher.java
@@ -0,0 +1,11 @@
+package frames;
+
+public class GELauncher {
+
+	public static void main(String[] args) {
+		GEMainFrame frame = GEMainFrame.getInstance();
+		frame.init();
+
+	}
+
+}
